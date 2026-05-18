9f9bced16f0f37b511d9ef4dd859d2cd657c2899
EK <ekliu@ieee.org>
2026-05-18 19:51:24 +0800

    update

--- streamlit-app.py
+++ streamlit-app.py
@@ -441,8 +441,6 @@ with col_right:
     st.markdown(f"""
     <div style="font-size: 0.9rem; color: #4b5563; line-height: 1;">
 
-    # {t('core_features')}
-
     </div>
     """, unsafe_allow_html=True)
 
